8f47df08e642ce78814c836d44132285fc094a7f
eventia <eventia@gmail.com>
2017-09-11 23:42:41 +0900

    Step 5: Shoot, Bunny, Shoot!

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-# Step 4: Turning the Bunny
+# Step 5: Shoot, Bunny, Shoot!
 # 1 - Import library
 import pygame
 from pygame.locals import *
@@ -10,11 +10,14 @@ width, height = 640, 480
 screen=pygame.display.set_mode((width, height))
 keys = [False, False, False, False]
 playerpos=[100,100]
+acc=[0,0]
+arrows=[]
 
 # 3 - Load images
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
 
 # 4 - keep looping through
 while 1:
@@ -39,6 +42,20 @@ while 1:
     playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]\
         -playerrot.get_rect().height/2)
     screen.blit(playerrot, playerpos1)
+
+    # 6.2 - Draw arrows
+    for bullet in arrows:
+        index=0
+        velx=math.cos(bullet[0])*10
+        vely=math.sin(bullet[0])*10
+        bullet[1]+=velx
+        bullet[2]+=vely
+        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+            arrows.pop(index)
+        index+=1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+            screen.blit(arrow1, (projectile[1], projectile[2]))
 
     # 7 - update the screen
     pygame.display.flip()
@@ -67,6 +84,12 @@ while 1:
                 keys[2]=False
             elif event.key==pygame.K_d:
                 keys[3]=False
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            position=pygame.mouse.get_pos()
+            acc[1]+=1
+            arrows.append([math.atan2(position[1]-(playerpos1[1]+32), \
+                position[0]-(playerpos1[0]+26)),playerpos1[0]+32,\
+                playerpos1[1]+32])
 
     # 9 - Move player
     if keys[0]:
@@ -83,7 +106,7 @@ while 1:
 # Step 2: Add Scenery [V]
 # Step 3: Make the Bunny Move [V]
 # Step 4: Turning the Bunny [V]
-# Step 5: Shoot, Bunny, Shoot!
+# Step 5: Shoot, Bunny, Shoot! [V]
 # Step 6: Take Up Arms! Badgers!
 # Step 7: Collisions with Badgers and Arrows
 # Step 8: Add a HUD with Health Meter and Clock
